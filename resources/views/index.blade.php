64173b60431c856d73f67d3d71ad3c7dbff0012c
yukimasaki <woodenhoe@gmail.com>
2022-08-03 13:46:29 +0900

    .editorconfig修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,8 @@
   <link rel="stylesheet" href="https://hypertext-candy.s3-ap-northeast-1.amazonaws.com/posts/vue-laravel-tutorial/app.css">
 </head>
 <body>
-  <div id="app"></div>
+  <div id="app">
+
+  </div>
 </body>
 </html>
