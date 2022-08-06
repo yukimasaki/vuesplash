7b32799a0b329179f8b2d6e4b82a0b10f1f13e14
yukimasaki <woodenhoe@gmail.com>
2022-08-06 14:01:18 +0900

    ログインAPI用ルート追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,9 +39,17 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function authenticated(Request $request, $user)
+        protected function authenticated(Request $request, $user)
     {
         return $user;
     }
 
+    protected function loggedOut(Request $request)
+    {
+        // セッションを再生成する
+        $request->session()->regenerate();
+    
+        return response()->json();
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,6 @@ Route::post('/register', 'Auth\RegisterController@register')->name('register');
 
 // ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
