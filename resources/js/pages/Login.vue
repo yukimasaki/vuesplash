3fc2882feeafdf86423e2b847d246875e545f78f
yukimasaki <woodenhoe@gmail.com>
2022-08-10 23:36:54 +0900

    タブUI追加

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Login</h1>
+  <div class="container--small">
+    <ul class="tab">
+      <li
+        class="tab__item"
+        @click="tab = 1"
+      >Login</li>
+      <li
+        class="tab__item"
+        @click="tab = 2"
+      >Register</li>
+    </ul>
+    {{ tab }}
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 1
+    }
+  }
+}
+</script>
