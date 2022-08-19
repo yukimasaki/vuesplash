e007a3c057e1684f2089f9807a05641069550848
yukimasaki <woodenhoe@gmail.com>
2022-08-19 22:29:36 +0900

    バリデーションエラー　ページコンポーネント修正

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -14,6 +14,14 @@
     </ul>
     <div class="panel" v-show="tab === 1">
       <form class="form" @submit.prevent="login">
+        <div v-if="loginErrors" class="errors">
+          <ul v-if="loginErrors.email">
+            <li v-for="msg in loginErrors.email" :key="msg">{{ msg }}</li>
+          </ul>
+          <ul v-if="loginErrors.password">
+            <li v-for="msg in loginErrors.password" :key="msg">{{ msg }}</li>
+          </ul>
+        </div>
         <label for="login-email">Email</label>
         <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
         <label for="login-password">Password</label>
@@ -42,6 +50,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data () {
     return {
@@ -58,11 +68,10 @@ export default {
       }
     }
   },
-  computed: {
-    apiStatus () {
-      return this.$store.state.auth.apiStatus
-    }
-  },
+  computed: mapState({
+      apiStatus: state => state.auth.apiStatus,
+      loginErrors: state => state.auth.loginErrorMessages
+  }),
   methods: {
     async login () {
       // authストアのloginアクションを呼び出す
@@ -79,7 +88,13 @@ export default {
 
       // トップページに移動する
       this.$router.push('/')
+    },
+    clearError () {
+      this.$store.commit('auth/setLoginErrorMessages', null)
     }
   },
+  created () {
+    this.clearError()
+  }
 }
 </script>
